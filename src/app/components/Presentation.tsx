fecdf6a555251bc6f5d1c423065e4b20adeeed91
MatiasStoroni <matias.storoni@gmail.com>
2025-06-18 20:50:09 -0300

    feat: update presentation component styles and content

--- src/app/components/Presentation.tsx
+++ src/app/components/Presentation.tsx
@@ -15,20 +15,20 @@ export default function Presentation() {
 
             {/* Content */}
             <div className="flex flex-col items-center relative z-10">
-                <div className="w-48 h-48 rounded-full border-4 border-white overflow-hidden flex items-center justify-center shadow-lg bg-white/80">
-                    <Image
-                        src="https://picsum.photos/200"
-                        alt="Profile"
-                        width={192}
-                        height={192}
-                        className="object-cover w-full h-full"
-                    />
+                <div className="w-48 h-48 rounded-full border-2 border-white overflow-hidden flex items-center justify-center shadow-lg bg-white/80">
+                    <Image src="/images/profile.jpeg" alt="Profile" width={192} height={192} className="object-cover w-full h-full" />
                 </div>
-                <h1 className="mt-8 text-4xl font-bold text-white drop-shadow-lg">Matias Storoni</h1>
+                <h1 className="mt-8 text-3xl font-bold text-white drop-shadow-lg">Matias Storoni</h1>
                 <h2 className="mt-2 text-xl font-medium text-white/80 drop-shadow">Ingeniero en Sistemas de Informacion - Desarrollador Full Stack Jr.</h2>
-                <div className="flex space-x-8 mt-5 text-white">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                <div className="flex flex-col md:flex-row gap-6 mt-5 w-3/5 text-white text-center text-lg bg-white/20 p-4 rounded-lg shadow-lg">
+                    <p className="bg-white/20 rounded-lg p-2 flex-1 flex items-center">
+                        Information Systems Engineer with a strong background in analysis, design, and development of web applications, complemented by
+                        knowledge in network administration, cybersecurity, and software projects management.
+                    </p>
+                    <p className="bg-white/20 rounded-lg p-2 flex-1 flex items-center">
+                        I am passionate about applying technology in practical ways to solve real-world problems, and I am committed to continuous
+                        learning—constantly researching and training in new tools to stay up-to-date in a rapidly evolving environment.
+                    </p>
                 </div>
             </div>
         </div>
